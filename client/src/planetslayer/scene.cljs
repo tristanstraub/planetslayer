cf6fdc8032dd974ea8fe5f9b0ac1f53343bcf567
Tristan Straub <tristanstraub@gmail.com>
2016-06-18 15:29:35 +1000

    Phong shading on sphere

--- client/src/planetslayer/scene.cljs
+++ client/src/planetslayer/scene.cljs
@@ -6,9 +6,7 @@
   (js/THREE.Vector3. (v 0) (v 1) (v 2)))
 
 (defn camera-move-to [camera pos]
-  ;; (set! (.. camera -position) (vec->threejs pos))
-  (set! (.. camera -position -z) 10)
-  )
+  (.. camera -position (set (pos 0) (pos 1) (pos 2))))
 
 (defn camera-look-at [camera pos]
   (.lookAt camera (vec->threejs pos)))
@@ -16,24 +14,30 @@
 (defn add-light [scene light]
   (.add scene light))
 
+(defn- light-move-to [light pos]
+  (.. light -position (set (pos 0) (pos 1) (pos 2))))
+
 (defn make-scene [window]
   (let [{:keys [width height]} window
         scene                  (js/THREE.Scene.)
         camera                 (js/THREE.PerspectiveCamera. 45 (/ width height)
                                                             0.1 2000)
-        light                  (js/THREE.AmbientLight. 0xffffff)]
-    (add-sphere scene)
-    (add-light scene light)
+        light                  (js/THREE.AmbientLight. 0x555555)
+        dlight                 (js/THREE.DirectionalLight. 0xff0000)]
+
     (camera-move-to camera [0 0 10])
     (camera-look-at camera [0 0 0])
+    (light-move-to dlight [-0.5 0 1])
+
+    (add-sphere scene)
+    (add-light scene light)
+    (add-light scene dlight)
 
     {:scene scene :camera camera}))
 
 (defn add-sphere [scene]
-  (let [mat  (js/THREE.MeshBasicMaterial. #js {:color 0xff0000})
+  (let [mat  (js/THREE.MeshPhongMaterial. #js {:color 0xff0000})
         geo  (js/THREE.SphereGeometry. 1 20 20)
         mesh (js/THREE.Mesh. geo mat)]
-
-
 
     (.add scene mesh)))
