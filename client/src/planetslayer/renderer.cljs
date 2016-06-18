25200e1ba80ca37d86dca52bedd1cfd6e8eadf44
Tristan Straub <tristanstraub@gmail.com>
2016-06-18 15:13:09 +1000

    Resizing!

--- client/src/planetslayer/renderer.cljs
+++ client/src/planetslayer/renderer.cljs
@@ -10,23 +10,6 @@
   {:width  (.-innerWidth js/window)
    :height (.-innerHeight js/window)})
 
-(defn resize! []
-  (println :resizing (get-window-size)))
-
-(def threejs
-  {:did-mount (fn [state]
-                (let [webgl (init-threejs!)]
-                  (.. js/document -body (appendChild (.-domElement webgl)))
-                  (.. js/window (addEventListener "resize" resize! false)))
-                (assoc state ::resize resize!))
-
-   :transfer-state (fn [o n]
-                     (assoc n ::resize (::resize o)))
-
-   :will-unmount (fn [state]
-                   (.. js/window (removeEventListener "resize" (::resize state)))
-                   state)})
-
 (defn resize-camera! [camera]
   (let [{:keys [width height]} (get-window-size)]
     (set! (.. camera -aspect) (/ width height))
@@ -35,6 +18,28 @@
 (defn resize-webgl! [webgl]
   (let [{:keys [width height]} (get-window-size)]
     (.setSize webgl width height)))
+
+(defn resizer [webgl]
+  (fn []
+    (resize-webgl! webgl)))
+
+(def threejs
+  {:did-mount (fn [state]
+                (let [webgl   (init-threejs!)
+                      resize! (resizer webgl)]
+                  (.. js/document -body (appendChild (.-domElement webgl)))
+                  (.. js/window (addEventListener "resize" resize! false))
+
+                  (resize!)
+
+                  (assoc state ::resize resize!)))
+
+   :transfer-state (fn [o n]
+                     (assoc n ::resize (::resize o)))
+
+   :will-unmount (fn [state]
+                   (.. js/window (removeEventListener "resize" (::resize state)))
+                   state)})
 
 (r/defc renderer-component < threejs []
   [:div])
